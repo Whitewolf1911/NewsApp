c7310fc8d861bb6b3ad1698ee559cf4d11c18624
Whitewolf1911 <alibasoglu1911@gmail.com>
2022-06-19 14:13:33 +0300

    Setup Retrofit

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "d53287002d734598b27e5639ea718f2e"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
